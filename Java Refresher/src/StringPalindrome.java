ac46b6993ecf4c5591d56354f31a55905c9c4b9e
Bryan Taylor <mcbtay@gmail.com>
2017-12-20 00:28:36 -0500

    Adding class StringPalindrome to determine if input is a palindrome

---
+++ Java Refresher/src/StringPalindrome.java
@@ -0,0 +1,7 @@
+/**
+ * A class where method(s) will be written to determine if a given string is a palindrome
+ * or not.  Ideally I'd like to solve it both iteratively and recursively.
+ */
+public class StringPalindrome {
+
+}
