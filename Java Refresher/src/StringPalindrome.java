b9e8157a4b004c941b5e129d58341e70a4e49b80
Bryan Taylor <mcbtay@gmail.com>
2017-12-20 00:49:00 -0500

    First pass at an iterative way to determine if a string is a palindrome

--- Java Refresher/src/StringPalindrome.java
+++ Java Refresher/src/StringPalindrome.java
@@ -3,5 +3,34 @@
  * or not.  Ideally I'd like to solve it both iteratively and recursively.
  */
 public class StringPalindrome {
-
+	
+	public static void main(String[] args) {
+		
+		StringPalindrome sp = new StringPalindrome();
+		
+		sp.IsPalindromeIterative("lololol");
+		sp.IsPalindromeIterative("bobby");
+		sp.IsPalindromeIterative("racecar");
+		sp.IsPalindromeIterative("grapes");
+	}
+	
+	public boolean IsPalindromeIterative(String input) {
+		System.out.print("Checking to see if " + input + " is a palindrome iteratively...");
+		char[] inputChars = input.toCharArray();
+		
+		int left = 0;
+		int right = inputChars.length - 1;
+		
+		while (right > left)
+		{
+			if (inputChars[left] != inputChars[right]) {
+				System.out.println("It isn't!");
+				return false;
+			}
+			left++;
+			right--;
+		}
+		System.out.println("It is!");
+		return true;
+	}
 }
