c5d7cf35f603b342a45e4b7ef64c16af97e1e704
Bryan Taylor <mcbtay@gmail.com>
2017-12-22 15:45:15 -0500

    Class to print and count the number of odds between 0 and n

---
+++ Java Refresher/src/PrintOdds.java
@@ -0,0 +1,43 @@
+/*
+ * Class with implementation to print all odd numbers between 1 and a specified number
+ */
+public class PrintOdds {
+
+	public int print(int n)
+	{
+		int numOdds = 0;
+		if (n < 1) return -1;
+		
+		for (int i = 1; i <= n; i++) {
+			if (i % 2 == 1) {
+				numOdds++;
+				System.out.print(" " + i);
+			}
+		}
+		System.out.print("...");
+		
+		return numOdds;
+	}
+	
+	public static void main(String[] args) {
+		PrintOdds odds = new PrintOdds();
+		int numOdds = 0;
+		
+		System.out.println("Printing odds between 1 and 99: ");
+		numOdds = odds.print(99);
+		System.out.println(numOdds + " total");
+		
+		System.out.println("Printing odds between 1 and 1: ");
+		numOdds = odds.print(1);
+		System.out.println(numOdds + " total");
+		
+		System.out.println("Printing odds between 1 and 35: ");
+		numOdds = odds.print(35);
+		System.out.println(numOdds + " total");
+		
+		System.out.println("Printing odds between 1 and 16: ");
+		numOdds = odds.print(16);
+		System.out.println(numOdds + " total");
+	}
+
+}
