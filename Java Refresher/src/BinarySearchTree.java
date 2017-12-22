b525497cfff82c72b2dc397dc3f5834dae0dcdfb
Bryan Taylor <mcbtay@gmail.com>
2017-12-22 12:09:50 -0500

    Implementing the insert method.  Next a method to display the tree

--- Java Refresher/src/BinarySearchTree.java
+++ Java Refresher/src/BinarySearchTree.java
@@ -8,7 +8,33 @@ public class BinarySearchTree {
 	}
 
 	public void insert(int n) {
-
+		Node newNode = new Node(n);
+		
+		// If root is null, insert our new node as the root and exit.
+		if (root == null) {
+			root = newNode;
+			return;
+		}
+		
+		Node current = root, parent = null;		
+		
+		// Loop over nodes until current == null, indicating we found our spot to insert
+		while (current != null) {
+			if (n > current.data) {
+				parent = current;
+				current = current.right;
+			} else if (n < current.data) {
+				parent = current;
+				current = current.left;
+			}
+		}
+		
+		// Check the parents data to ensure we insert on the correct side
+		if (newNode.data > parent.data) {
+			parent.right = newNode;
+		} else if (newNode.data < parent.data) {
+			parent.left = newNode;
+		}
 	}
 	
 	public boolean find(int n) {
