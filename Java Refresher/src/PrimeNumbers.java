a8f37302723b6668c0ae7eef38c0d69cedd347e7
Bryan Taylor <mcbtay@gmail.com>
2017-12-26 11:36:06 -0500

    start to a class to print out primes between 1 and n

---
+++ Java Refresher/src/PrimeNumbers.java
@@ -0,0 +1,15 @@
+/*
+ * A method to print all the primes between 1 and n.
+ */
+public class PrimeNumbers {
+	
+	public static void main(String[] args) {
+		PrimeNumbers primes = new PrimeNumbers();
+		
+		primes.primesIn(88);
+		primes.primesIn(44);
+		primes.primesIn(365);
+		primes.primesIn(7);
+	}
+
+}
