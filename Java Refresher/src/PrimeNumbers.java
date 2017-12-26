84e89820107750e8f665add89248f8c566e3449f
Bryan Taylor <mcbtay@gmail.com>
2017-12-26 11:47:34 -0500

    implementation to print the primes

--- Java Refresher/src/PrimeNumbers.java
+++ Java Refresher/src/PrimeNumbers.java
@@ -1,7 +1,29 @@
+import java.util.HashMap;
+
 /*
  * A method to print all the primes between 1 and n.
  */
 public class PrimeNumbers {
+	
+	public void primesIn(int n) {
+		System.out.print("Primes in " + n + ": ");
+		// For each number between 1 and n
+		for (int i = 2; i < n; i++) {
+			int count = 0;
+			
+			for (int j = 2; j <= i/2; j++) {
+				
+				if (i % j == 0) {
+					count++;
+				}
+			}
+			
+			if (count == 0) {
+				System.out.print(i + " ");
+			}
+		}
+		System.out.println("");
+	}
 	
 	public static void main(String[] args) {
 		PrimeNumbers primes = new PrimeNumbers();
