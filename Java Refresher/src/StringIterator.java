1c5d2f569fad6db666750eff4a95bfbaccbb7a0d
Bryan Taylor <mcbtay@gmail.com>
2017-12-26 11:15:59 -0500

    new class with some string iteration

---
+++ Java Refresher/src/StringIterator.java
@@ -0,0 +1,15 @@
+/* 
+ * Class to iterate over a string and spit out each character one by one.
+ */
+public class StringIterator {
+
+	
+	
+	public static void main (String[] args) {
+		StringIterator si = new StringIterator();
+		
+		si.iterate("my cool string");
+		si.iterate("Bryan");
+		si.iterate("Winter is coming...");
+	}
+}
