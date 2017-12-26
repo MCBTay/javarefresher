55c4aafb51b3f2ab240cb7a1edf6d0e4c7a75fc4
Bryan Taylor <mcbtay@gmail.com>
2017-12-26 14:36:13 -0500

    StringBuffer and iterative implementations

--- Java Refresher/src/StringReverse.java
+++ Java Refresher/src/StringReverse.java
@@ -10,17 +10,25 @@ public class StringReverse {
 	}
 	
 	public void reverseAllTheWays() {
+		System.out.println("Original string: " + myString);
 		reverseWithStringBuffer();
 		reverseIteratively();
 		reverseRecursively();
 	}
 	
 	public void reverseWithStringBuffer() {
-		
+		System.out.println("StringBuffer reverse: " + new StringBuffer(myString).reverse().toString());
 	}
 	
 	public void reverseIteratively() {
+		char[] chars = myString.toCharArray();
+		StringBuilder sb = new StringBuilder();
 		
+		for (int i = myString.length() - 1; i >= 0; i--) {
+			sb.append(chars[i]);
+		}
+		
+		System.out.println("Iterative reverse: " + sb.toString());
 	}
 	
 	public void reverseRecursively() {
